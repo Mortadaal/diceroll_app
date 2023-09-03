598ec95c689d26d91ae24effc3b10bd156ad4e23
Mortadaal <47819210+Mortadaal@users.noreply.github.com>
2023-09-03 11:34:45 +0200

    changede it from var to const

--- diceroll_app/lib/gradient_container.dart
+++ diceroll_app/lib/gradient_container.dart
@@ -3,16 +3,16 @@ import 'package:diceroll_app/text_style2.dart';
 
 //widgets for at gøre ens kode mindre 
 //hvilket også giver mulighed for at genbruge
-var startAlignment =Alignment.topLeft;
-var endAlignment=Alignment.bottomRight;
+const startAlignment =Alignment.topLeft;
+const endAlignment=Alignment.bottomRight;
 class GradientContainer extends StatelessWidget{
  const GradientContainer({super.key});
  @override
   Widget build(context){
     return Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
-              colors: const [
+              colors:  [
                 Color.fromARGB(255, 150, 244, 154),
                 Color.fromARGB(255, 150, 208, 244),
               ],
