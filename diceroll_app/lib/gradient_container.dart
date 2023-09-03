8248b7b883b900411cb906b7a0cbe08bc9b7257f
Mortadaal <47819210+Mortadaal@users.noreply.github.com>
2023-09-03 11:32:45 +0200

    created variable for alignment

--- diceroll_app/lib/gradient_container.dart
+++ diceroll_app/lib/gradient_container.dart
@@ -3,20 +3,21 @@ import 'package:diceroll_app/text_style2.dart';
 
 //widgets for at gøre ens kode mindre 
 //hvilket også giver mulighed for at genbruge
-
+var startAlignment =Alignment.topLeft;
+var endAlignment=Alignment.bottomRight;
 class GradientContainer extends StatelessWidget{
  const GradientContainer({super.key});
  @override
   Widget build(context){
     return Container(
-          decoration: const BoxDecoration(
+          decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors: [
+              colors: const [
                 Color.fromARGB(255, 150, 244, 154),
                 Color.fromARGB(255, 150, 208, 244),
               ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+              begin: startAlignment,
+              end: endAlignment,
             ),
           ),
           child: const Center(
